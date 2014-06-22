4240ad8eb27e1a0076530f145cbb3cdb46fc7c11
Ambuj Kumar <sendambuj@gmail.com>
2014-06-22 08:04:27 -0400

    Update PrimerCheck.py

--- PrimerCheck.py
+++ PrimerCheck.py
@@ -18,7 +18,10 @@
 # under certain conditions;                                                                                    #
 #                                                                                                              #
 ################################################################################################################
-
+"""Code requires local protein sequence database/file to perform Blastx operation. 
+This executes for OR database which I obtained from Johny Wright, PhD student, 
+Biology Department, University of Florida. A slight change in database name supplied 
+in line 142 will do well for others"""
 
 
 import os
